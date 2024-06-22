2a2f665c7de52434b4dbaa93b3b09330391c1937
wert2024 <158856496+wert2024@users.noreply.github.com>
2024-06-22 08:00:21 +0300

    Add files via upload

---
+++ калькулятор.js
@@ -0,0 +1,38 @@
+jQuery('document').ready(function(){
+
+
+	$.post (
+		"https://wert2024.github.io/Time1/",
+		{
+			"num1" : 3,
+			"num2" : 12
+		},
+		function(data){
+			alert(data);
+		}
+	);
+	
+
+	
+
+     
+     
+     });
+ 
+     
+     
+
+
+
+  
+     
+
+
+
+
+
+
+
+
+
+
